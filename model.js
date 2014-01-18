d4f30b50582abdaf1b3964ed75a6a8f81f387d60
mmotel <mateuszmotel@gmail.com>
2014-01-18 01:03:52 +0100

    fixed tabs formating

--- model.js
+++ model.js
@@ -30,40 +30,40 @@ Tasks = new Meteor.Collection('tasks');
 Lists = new Meteor.Collection('lists');
 
 Tasks.allow({
-	'insert': function (userId, task){
-		return false; //use Meteor.methods
-	},
-	'update': function (userId, task, fields, modifier){
-		var allowed = [ "name" ];
-		return (task.owner === userId) && (_.difference(fields, allowed).length === 0);
-	},
-	'remove': function (userId, task){
-		return task.owner === userId;
-	},
+  'insert': function (userId, task){
+    return false; //use Meteor.methods
+  },
+  'update': function (userId, task, fields, modifier){
+    var allowed = [ "name" ];
+    return (task.owner === userId) && (_.difference(fields, allowed).length === 0);
+  },
+  'remove': function (userId, task){
+    return task.owner === userId;
+  },
 });
 
 Lists.allow({
-	'insert': function (userId, list){
-		return false; //use Meteor.methods 
-	},
-	'update': function (userId, list, fields, modifier){
-		var allowed = [ "name" ];
-		return (list.owner === userId) && (_.difference(fields, allowed).length === 0);
-	},
-	'remove': function (userId, list){
-		return false; //use Meteor.methods
-	},
+  'insert': function (userId, list){
+    return false; //use Meteor.methods 
+  },
+  'update': function (userId, list, fields, modifier){
+    var allowed = [ "name" ];
+    return (list.owner === userId) && (_.difference(fields, allowed).length === 0);
+  },
+  'remove': function (userId, list){
+    return false; //use Meteor.methods
+  },
 });
 
 Meteor.methods({
-	addList: function (name, owner){
-		Lists.insert(new List(name, owner));
-	},
-	rmList: function (listid){
-      Lists.remove({_id: listid});
-      Tasks.remove({"listid": listid});
-	},
-	addTask: function (name, owner, listid){
-		Tasks.insert(new Task(name, owner, listid));
-	}
+  addList: function (name, owner){
+    Lists.insert(new List(name, owner));
+  },
+  rmList: function (listid){
+    Lists.remove({_id: listid});
+    Tasks.remove({"listid": listid});
+  },
+  addTask: function (name, owner, listid){
+    Tasks.insert(new Task(name, owner, listid));
+  }
 });
